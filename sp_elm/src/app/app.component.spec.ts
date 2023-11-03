e1f48ab66d5a9abbef03a4d654ba6c0e93779edf
FATTYBUN_ASUS\jacky <jackylau001128@gmail.com>
2023-11-03 14:35:56 +0800

    basic setup and installation for user and admin front-end

---
+++ sp_elm/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'sp_elm'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('sp_elm');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sp_elm app is running!');
+  });
+});
---
+++ sp_elm/src/app/favourite/favourite.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-favourite',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './favourite.component.html',
+  styleUrls: ['./favourite.component.scss']
+})
+export class FavouriteComponent {
+
+}
