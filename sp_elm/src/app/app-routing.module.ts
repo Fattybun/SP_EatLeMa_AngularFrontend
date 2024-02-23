54daaf5d8a8efa887b975bcc581cd57237cdc323
FATTYBUN_ASUS\jacky <jackylau001128@gmail.com>
2024-02-23 15:15:09 +0800

    Solved node package conflict

--- sp_elm/src/app/app-routing.module.ts
+++ sp_elm/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { HomeComponent } from './home/home.component';
+import { authGuard } from './services/auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -10,7 +11,8 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    component: HomeComponent
+    canActivate: [authGuard],
+    component: HomeComponent,
   }
 ];
 
--- sp_elm/src/app/services/auth/auth.service.ts
+++ sp_elm/src/app/services/auth/auth.service.ts
@@ -3,7 +3,24 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
   constructor() { }
+
+  userSignIn() {
+    fetch('http://localhost:3001/signin', { method: "POST" })
+      .then(response => response.json())
+      .then(data => data)
+      .catch((error) => console.log(`Error: ${error}`));
+  }
+  
+  userSignUp() {}
+
+  userLogout() {}
+
+  isLoggedIn() {}
+
+  resetPassword() {}
+
 }
